05bd4858cf549c1701f3e6cad1b3536e70492a01
Ian Pollack <ianjpollack@gmail.com>
2018-01-25 05:55:38 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,7 +16,7 @@ function doWhileLoop(array) {
     function maybeTrue() {
       return Math.random() >= 0.5
     }
-    do {array.pop}
+    do {array = array.pop}
       while (array.length > 0 && maybeTrue())
   return array}
   
